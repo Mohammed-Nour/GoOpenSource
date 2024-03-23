4d7860ab9dee21e2af330b0d51c814473ac3ab18
Aymen <aymounadaassi@gmail.com>
2024-03-23 22:52:17 +0300

    add fix

--- front/GoOpenSourcePlatform/src/components/Home/Carousel.vue
+++ front/GoOpenSourcePlatform/src/components/Home/Carousel.vue
@@ -6,13 +6,15 @@
                 <div class="border-1 surface-border border-round m-2  p-3">
                     <div class="mb-3">
                         <div class="relative mx-auto">
-                            <img :src="'https://primefaces.org/cdn/primevue/images/product/' + slotProps.data.image" :alt="slotProps.data.name" class="w-full border-round" />
-                            <Tag :value="slotProps.data.inventoryStatus" :severity="getSeverity(slotProps.data.inventoryStatus)" class="absolute" style="left:5px; top: 5px"/>
+                            <!-- <img :src="'https://primefaces.org/cdn/primevue/images/product/' + slotProps.data.image" :alt="slotProps.data.name" class="w-full border-round" /> -->
+                            <img src="../../assets/images/Home/dummy-carousel.svg" :alt="slotProps.data.name" class=" w-25rem border-round" />
+
+                            <!-- <Tag :value="slotProps.data.inventoryStatus" :severity="getSeverity(slotProps.data.inventoryStatus)" class="absolute" style="left:5px; top: 5px"/> -->
                         </div>
                     </div>
-                    <div class="mb-3 font-medium">{{ slotProps.data.name }}</div>
-                    <div class="flex justify-content-between align-items-center">
-                        <div class="mt-0 font-semibold text-xl">${{ slotProps.data.price }}</div>
+                    <div class="BLACK mb-3 font-medium text-xl">Open Source Project</div>
+                    <div class="BLACK flex justify-content-between align-items-center">
+                        <div class="mt-0 font-regular text-m">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
                         <span>
                             <Button icon="pi pi-heart" severity="secondary" outlined />
                             <Button icon="pi pi-shopping-cart" class="ml-2"/>
@@ -77,3 +79,12 @@ export default {
     }
 };
 </script>
+
+
+<style scoped>
+.BLACK {
+    color: black;
+
+}
+
+</style>
--- front/GoOpenSourcePlatform/src/components/Home/Hero1.vue
+++ front/GoOpenSourcePlatform/src/components/Home/Hero1.vue
@@ -3,8 +3,8 @@
 <div class="grid grid-nogutter surface-section text-800">
     <div class="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center ">
         <section>
-            <span class="block text-6xl font-bold mb-1">Create the screens your</span>
-            <div class="text-6xl text-primary font-bold mb-3">visitors deserve to see</div>
+            <span class="block text-6xl font-bold mb-1">Contribute to Open Source Projects without limits</span>
+            <div class="text-3xl text-primary font-bold mb-3">access the larget community of open source enthusiasts</div>
             <p class="mt-0 mb-4 text-700 line-height-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             
             <Button label="Learn More" type="button" class="mr-3 p-button-raised"></Button>
